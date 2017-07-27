50e10ce5bdf08d91b0d63e8f9b540bfb28959f98
Miguel Bertaud <miguel.bertaud@gmail.com>
2017-07-27 13:47:49 -0500

    Add home controller for users

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -136,7 +136,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  config.reconfirmable = false
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
@@ -251,7 +251,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET']
+  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'],
+  {
+    verify_iss: false
+  }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
